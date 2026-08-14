84e8dc698e9eea9f5c161c545813629e1178f94a
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-14 15:06:58 +0530

    File updated at 14/08/2026, 15:06:58

--- https:/github.com/V0367/LeetCode.git/53-MaximumSubarray/53-MaximumSubarray.py
+++ https:/github.com/V0367/LeetCode.git/53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,16 +1,11 @@
-# Last updated: 14/08/2026, 14:52:39
+# Last updated: 14/08/2026, 15:06:58
 1class Solution(object):
 2    def maxSubArray(self, nums):
 3        current_sum = nums[0]
-4        max_sum = max(nums)
+4        max_sum = nums[0]
 5
 6        for i in range(1,len(nums)):
-7            if current_sum+nums[i]<=nums[i]:
-8                current_sum = nums[i]
-9            else:
-10                current_sum+=nums[i]
-11
-12            if max_sum<current_sum:
-13                max_sum=current_sum
-14        return max_sum
-15
+7            current_sum = max(nums[i], current_sum+nums[i])
+8            max_sum = max(max_sum, current_sum)
+9        return max_sum
+10

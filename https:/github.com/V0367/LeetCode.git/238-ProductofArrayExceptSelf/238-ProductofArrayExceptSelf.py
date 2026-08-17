5a268b0cc43be69aed5677c8d21de1a62a6faed3
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-17 11:15:37 +0530

    File updated at 17/08/2026, 11:15:36

--- https:/github.com/V0367/LeetCode.git/238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ https:/github.com/V0367/LeetCode.git/238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,17 +1,15 @@
-# Last updated: 17/08/2026, 11:06:21
+# Last updated: 17/08/2026, 11:15:36
 1class Solution(object):
 2    def longestConsecutive(self, nums):
-3        c_count = 0
-4        max_count = 0
-5        new = set(nums)
-6        n = len(new)
-7        for x in new:
-8            if x-1 not in new:
-9                current = x
-10                c_count = 1
-11                while current + 1 in new:
-12                    current +=1
-13                    c_count+=1
-14            max_count = max(max_count,c_count)
-15        return max_count
-16        
+3        nums_set = set(nums)
+4        longest = 0
+5        for x in nums_set:
+6            if x-1 not in nums_set:
+7                current = x
+8                length = 1
+9                while current+1 in nums_set:
+10                    current+=1
+11                    length +=1
+12                longest = max(longest,length)
+13        return longest
+14        

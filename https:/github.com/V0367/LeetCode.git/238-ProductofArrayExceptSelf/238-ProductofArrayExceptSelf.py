de789a739c0e2ca44558ab7a0242e7054ab25445
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-17 11:06:22 +0530

    File updated at 17/08/2026, 11:06:21

--- https:/github.com/V0367/LeetCode.git/238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ https:/github.com/V0367/LeetCode.git/238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,21 +1,17 @@
-# Last updated: 17/08/2026, 10:28:50
+# Last updated: 17/08/2026, 11:06:21
 1class Solution(object):
-2    def productExceptSelf(self, nums):
-3        n = len(nums)
-4        answer = [1]*n
-5
-6        left_product = 1
-7
-8        for i in range(n):
-9            answer[i] = left_product
-10            left_product *= nums[i]
-11
-12        right_product = 1
-13
-14        for i in range(n-1,-1,-1):
-15            answer[i] *= right_product
-16            right_product *= nums[i]
-17        
-18        return answer
-19            
-20        
+2    def longestConsecutive(self, nums):
+3        c_count = 0
+4        max_count = 0
+5        new = set(nums)
+6        n = len(new)
+7        for x in new:
+8            if x-1 not in new:
+9                current = x
+10                c_count = 1
+11                while current + 1 in new:
+12                    current +=1
+13                    c_count+=1
+14            max_count = max(max_count,c_count)
+15        return max_count
+16        

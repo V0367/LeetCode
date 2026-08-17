ef69144b6fcbe14446e869b340e6c04dfc2ef11e
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-17 16:35:38 +0530

    File updated at 17/08/2026, 16:35:37

--- https:/github.com/V0367/LeetCode.git/268-MissingNumber/268-MissingNumber.py
+++ https:/github.com/V0367/LeetCode.git/268-MissingNumber/268-MissingNumber.py
@@ -1,14 +1,11 @@
-# Last updated: 17/08/2026, 16:30:14
+# Last updated: 17/08/2026, 16:35:37
 1class Solution(object):
 2    def missingNumber(self, nums):
 3        """
 4        :type nums: List[int]
 5        :rtype: int
 6        """
-7        for i in nums:
-8            if i-1>=0 and i-1 not in nums: 
-9                return i-1
-10                
-11            elif i+1<=len(nums) and i+1 not in nums:
-12                return i+1
-13                       
+7        for i in range(len(nums)+1):
+8            if i not in nums:
+9                return i
+10                       

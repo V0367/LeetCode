b9ff6ae20073b46a8d5e5a029ebba17126f3129f
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-20 11:45:34 +0530

    File updated at 20/08/2026, 11:45:34

--- https:/github.com/V0367/LeetCode.git/448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ https:/github.com/V0367/LeetCode.git/448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,16 +1,19 @@
-# Last updated: 20/08/2026, 11:13:24
+# Last updated: 20/08/2026, 11:45:34
 1class Solution(object):
-2    def findDisappearedNumbers(self, nums):
-3        n = len(nums)
-4        missing = []
-5        for x in nums:
-6            index = abs(x) - 1
-7            nums[index] = -abs(nums[index])
-8        
-9        for i in range(n):
-10            if nums[i]>0:
-11                missing.append(i+1)
-12        return missing
-13                
-14        
-15        
+2    def moveZeroes(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: None Do not return anything, modify nums in-place instead.
+6        """
+7        position = 0
+8        n = len(nums)
+9        for x in nums:
+10            if x!=0:
+11                nums[position] = x
+12                position+=1
+13            
+14        for i in range(position,n):
+15            nums[i] = 0
+16
+17        return nums
+18        

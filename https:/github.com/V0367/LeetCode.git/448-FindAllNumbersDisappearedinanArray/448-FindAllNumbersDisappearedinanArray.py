808d24fbab07565a91efc7773afc9d77bde4f6cc
V0367 <168622872+V0367@users.noreply.github.com>
2026-08-20 11:13:25 +0530

    File updated at 20/08/2026, 11:13:24

--- https:/github.com/V0367/LeetCode.git/448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ https:/github.com/V0367/LeetCode.git/448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,17 +1,16 @@
-# Last updated: 20/08/2026, 11:12:49
+# Last updated: 20/08/2026, 11:13:24
 1class Solution(object):
 2    def findDisappearedNumbers(self, nums):
 3        n = len(nums)
 4        missing = []
 5        for x in nums:
 6            index = abs(x) - 1
-7            if nums[index]>0:
-8                nums[index] = -nums[index]
-9        
-10        for i in range(n):
-11            if nums[i]>0:
-12                missing.append(i+1)
-13        return missing
-14                
+7            nums[index] = -abs(nums[index])
+8        
+9        for i in range(n):
+10            if nums[i]>0:
+11                missing.append(i+1)
+12        return missing
+13                
+14        
 15        
-16        

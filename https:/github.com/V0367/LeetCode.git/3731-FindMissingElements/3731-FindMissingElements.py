f923eb5f7e7bd016c9d4b5a7dc929eb3d149898b
V0367 <168622872+V0367@users.noreply.github.com>
2026-09-04 17:12:03 +0530

    File updated at 04/09/2026, 17:12:04

--- https:/github.com/V0367/LeetCode.git/3731-FindMissingElements/3731-FindMissingElements.py
+++ https:/github.com/V0367/LeetCode.git/3731-FindMissingElements/3731-FindMissingElements.py
@@ -1,4 +1,4 @@
-# Last updated: 04/09/2026, 17:10:59
+# Last updated: 04/09/2026, 17:12:04
 1class Solution(object):
 2    def findMissingElements(self, nums):
 3        
@@ -6,9 +6,8 @@
 5        nums = sorted(nums)
 6        n = len(nums)
 7
-8
-9        for i in range(nums[0],nums[n-1]):
-10            if i not in nums:
-11                missing.append(i)
-12        return missing 
-13        
+8        for i in range(nums[0],nums[n-1]):
+9            if i not in set(nums):
+10                missing.append(i)
+11        return missing 
+12        
